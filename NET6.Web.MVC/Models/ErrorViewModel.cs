a416aa783d7978f741a0c4bf03e6a5f6d6c91023
Stone <m18146611430@163.com>
2021-12-19 15:03:42 +0800

    添加项目文件。

---
+++ NET6.Web.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NET6.Web.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
