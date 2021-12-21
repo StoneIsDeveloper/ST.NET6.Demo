14292d30c98fb0006dbdacad23267e792f82ff23
Stone <m18146611430@163.com>
2021-12-21 23:41:53 +0800

    add mvc

---
+++ NET6.Console.Demo/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace NET6.Console.Demo.Model
+{
+    record  struct Person
+    {
+        public string FirstName { get; init; } = "Jone";
+        public string LastName { get; init; } = "Doe";
+        public void WriteToFile(string filePath)
+            => File.WriteAllText(filePath, this.ToString());
+    }
+}
---
+++ NET6.Web.MVC/Controllers/FirstController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NET6.Web.MVC.Controllers
+{
+    public class FirstController : Controller
+    {
+        // GET: FirstController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
