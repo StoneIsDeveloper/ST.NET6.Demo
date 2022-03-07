9db11c0d6ccf543c16f2c41b05b1c0b4a93e9fc3
stoneisdeveloper <m18146611430@163.com>
2022-03-07 13:37:55 +0800

    Add Codility

---
+++ ColilityTest/CenterUility.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodilityTest
+{
+    public class CenterUility
+    {
+        public static int FindMaxBinaryGap(int number)
+        {
+            {
+                var number1 = 17;
+                // 10000  16
+                // 10001  17
+                var n1 = number1 / 2;
+                var n2 = number1 % 2;
+            }
+
+            var findOne = false;
+            var maxGap = 0;
+            var zeroCount = 0;
+            var divResult = number;
+
+            while (divResult > 0)
+            {
+                var remainder = 0;
+                divResult = Math.DivRem(divResult, 2, out remainder);  // 取整 & 取余
+                if (remainder == 1)
+                {
+                    findOne = true;
+                    zeroCount = 0;
+                }
+                else if (findOne)
+                {
+                    zeroCount++;
+                    if (zeroCount > maxGap)
+                        maxGap = zeroCount;
+                }
+
+            }
+
+            return maxGap;
+        }
+    }
+}
---
+++ ColilityTest/Program.cs
@@ -0,0 +1,11 @@
+﻿using CodilityTest;
+
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+// 1. Find Max gap in Binary
+CenterUility.FindMaxBinaryGap(4);
+
