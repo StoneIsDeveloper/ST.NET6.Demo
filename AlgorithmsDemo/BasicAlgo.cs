140d3783a4d9d830bc16c331f85184068f1b32ab
li3963li2072 <stoneisdeveloper>
2022-04-02 11:19:19 +0800

    归并排序 V2 不用哨兵

--- AlgorithmsDemo/BasicAlgo.cs
+++ AlgorithmsDemo/BasicAlgo.cs
@@ -43,7 +43,7 @@ namespace AlgorithmsDemo
             int n1 = p2 - p1 + 1; //4
             int n2 = r - p2;  // 4
             int[] left = new int[n1 + 1];   //4, 2, 7, 5
-            int[] rigth = new int[n2 + 1];  //1, 3, 6, 2 
+            int[] right = new int[n2 + 1];  //1, 3, 6, 2 
 
             for (int i = 0; i < n1; i++)
             {
@@ -51,11 +51,11 @@ namespace AlgorithmsDemo
             }
             for (int j = 0; j < n2; j++)
             {
-                rigth[j] = a[p2 + j];
+                right[j] = a[p2 + j];
             }
 
             left[n1] = int.MaxValue;
-            rigth[n2] = int.MaxValue;
+            right[n2] = int.MaxValue;
 
             int ii = 0;
             int jj = 0;
@@ -63,17 +63,96 @@ namespace AlgorithmsDemo
             for (int k = p1; k <= r; k++)
             {
 
-                if (left[ii] <= rigth[jj])
+                if (left[ii] <= right[jj])
                 {
                     a[k-1] = left[ii];
                     ii++;
                 }
                 else
                 {
-                    a[k-1] = rigth[jj];
+                    a[k-1] = right[jj];
                     jj++;
                 }
 
+
+            }
+
+            return a;
+
+        }
+
+
+        public static void MergeSortV2(int[] a, int p, int r)
+        {
+            if (p < r)
+            {
+                int q = (p + r) / 2;  // 8/2 =4  9/2 = 4
+
+                // 递归式 分解 
+                MergeSortV2(a, p, q);  // left
+                MergeSortV2(a, q + 1, r); //right
+
+                //自低向上 归并排序
+                MergeV2(a, p, q, r);
+            }
+        }
+        /// <summary>
+        /// 不适用哨兵
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="p1"></param>
+        /// <param name="p2"></param>
+        /// <param name="r"></param>
+        /// <returns></returns>
+        public static int[] MergeV2(int[] a, int p1, int p2, int r)
+        {
+            var result = new int[8];
+            int n1 = p2 - p1 + 1; //4
+            int n2 = r - p2;  // 4
+            int[] left = new int[n1];   //4, 2, 7, 5
+            int[] right = new int[n2];  //1, 3, 6, 2 
+
+            for (int i = 0; i < n1; i++)
+            {
+                left[i] = a[p1 + i - 1];
+            }
+            for (int j = 0; j < n2; j++)
+            {
+                right[j] = a[p2 + j];
+            }
+
+
+            int ii = 0;
+            int jj = 0;
+
+            //如果一个数组已全部复制回A, 就把另一个数组剩余的元素全部复制回A
+
+            for (int k = p1; k <= r; k++)
+            {
+                if(ii == left.Length && jj < right.Length)// left is done, then clone right
+                {
+                    a[k - 1] = right[jj];
+                    jj++;
+                }
+                if (jj == right.Length && ii< left.Length )// right is done, then clone left
+                {
+                    a[k - 1] = left[ii];
+                    ii++;
+                }
+   
+                if(ii< left.Length && jj< right.Length)
+                {
+                    if (left[ii] <= right[jj])
+                    {
+                        a[k - 1] = left[ii];
+                        ii++;  // 0 1 2 3
+                    }
+                    else
+                    {
+                        a[k - 1] = right[jj];
+                        jj++; // 0 1 2 3
+                    }
+                }              
 
             }
 
--- AlgorithmsDemo/Program.cs
+++ AlgorithmsDemo/Program.cs
@@ -12,12 +12,21 @@ Console.WriteLine("Hello, World!");
     int r = a.Length;
 
     BasicAlgo.MergeSort(a, p, r);
+
+    var a2 = new int[8] { 9, 8, 7, 6, 1, 4, 3, 2 };
+    int p2 = 1;
+    int r2 = a.Length;
+
+    BasicAlgo.MergeSortV2(a2, p2, r2);
 }
 
 
 {
     var b = new int[6] { 5, 2, 4, 6, 1, 3 };
     BasicAlgo.InsertionSort(b);
+
+    var e = new int[6] { 5, 2, 4, 6, 1, 3 };
+    BasicAlgo.InsertionSort(e);
 
 }
 
